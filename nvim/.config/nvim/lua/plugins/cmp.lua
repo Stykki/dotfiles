54dd5037522a279764d8c5cd02b9c6f933cb35a5
stykki <panoari@gmail.com>
2024-05-18 23:49:46 +0000

    Update configs

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -1,6 +1,7 @@
 return {
 	{
 		"hrsh7th/nvim-cmp",
+		lazy = false,
 		event = "InsertEnter",
 		dependencies = {
 			{
@@ -48,7 +49,21 @@ return {
 		},
 		config = function()
 			local cmp = require("cmp")
+
 			local opts = {
+
+				snippet = {
+					-- REQUIRED - you must specify a snippet engine
+					expand = function(args)
+						-- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+						require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+						-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+						-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+						-- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v1.10+)
+					end,
+				},
+
+
 				completion = {
 					completeopt = "menu,menuone",
 				},
--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -1,10 +1,20 @@
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
 return {
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
 			local lspconfig = require("lspconfig")
 			lspconfig.rust_analyzer.setup {
-				capabilities = vim.lsp.protocol.make_client_capabilities()
+				capabilities = vim.lsp.protocol.make_client_capabilities(),
+				filetypes = { "rust" },
+				settings = {
+					['rust-analyzer'] = {
+						cargo = {
+							allFeatures = true
+						}
+					}
+				}
 			}
 
 			lspconfig.lua_ls.setup {
@@ -28,6 +38,15 @@ return {
 			lspconfig.tailwindcss.setup {
 				capabilities = vim.lsp.protocol.make_client_capabilities()
 			}
+
+			lspconfig.zls.setup {
+				capabilities = vim.lsp.protocol.make_client_capabilities()
+			}
+
+		lspconfig.openscad_lsp.setup {
+				capabilities = capabilities
+			}
+
 		end,
 	},
 }
